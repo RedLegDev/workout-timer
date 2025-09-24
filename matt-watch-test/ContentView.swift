e441dfcc780b5dcdb511a12b18317f14860db6c7
mattgwagner <Matt@RedLeg.dev>
2025-09-24 15:48:01 -0400

    Initial Commit

---
+++ matt-watch-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  matt-watch-test
+//
+//  Created by Matt Wagner on 9/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ matt-watch-test/matt_watch_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  matt_watch_testApp.swift
+//  matt-watch-test
+//
+//  Created by Matt Wagner on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct matt_watch_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
